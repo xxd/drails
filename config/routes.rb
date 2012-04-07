8aa11d0d9ba35b67f994482947e979f2b91d14cb
xxd <xxdxxd51@hotmail.com>
2012-04-07 16:31:24 +0800

    fork done but with error "ActionView::MissingTemplate"

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Dandan::Application.routes.draw do
       get :sync
     end
   end
+  
+  match "items/:origin_id/fork/:list_id" => "Items#fork", :via => "post"
   resources :items, :only => [:index, :show, :create] do
     member do
       get :sync
